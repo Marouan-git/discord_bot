67eb82d611f3abe9530a7fc233c26247a0bac29f
Marouan <marouan.boulli.simplon@gmail.com>
2022-04-27 09:38:06 +0200

    add discord bot

---
+++ bot.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+from functions import get_intent, get_sunset, get_temp, select_learner
+
+load_dotenv()
+
+luis_key = os.getenv("LUIS_API_KEY")
+luis_url = os.getenv("LUIS_REQUEST")
+weather_key = os.getenv("WEATHER_API_KEY")
+learners = ["Thienvu","Marouan","Armand","Gabriel","Merouane","Aissa","David","Cinthya","Cyrille","Rayanne","William","Thomas","Kamel","Nolan","Pierre-Loic"]
+key = os.getenv("DISCORD_KEY")
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Le bot est prêt !")
+
+@client.event
+async def on_message(message):
+    
+    if message.author == client.user:
+        return
+    
+    if client.user.mentioned_in(message):   
+        
+        luis_response = get_intent(luis_key, luis_url, message.content)
+        intent = luis_response["intent"]
+        entity = luis_response["entity"]
+            
+        if luis_response["score"] > 0.5:
+            if intent == "connaitre la température":
+                temp = get_temp(weather_key, entity)["temp"]
+                await message.channel.send(f"Température Celsius à {entity} : {temp}")
+            elif intent == "connaitre coucher soleil":
+                sunset = get_sunset(weather_key, entity)['sunset']
+                await message.channel.send(f"Coucher de soleil à {entity} : {sunset}")
+            elif intent == "trouver un apprenant pour présenter son travail":
+                await message.channel.send(f"{select_learner(learners)} doit présenter son travail !")
+        elif 'Hello' or 'Hi' or 'Bonjour' in message.content:
+            await message.channel.send(f"Hello!")
+        else:
+            await message.channel.send(f"Vous avez dit '{ message.content }'. je n'ai pas bien compris...")
+
+client.run(key)
---
+++ functions.py
@@ -0,0 +1,58 @@
+import requests
+import os
+import urllib
+from datetime import datetime
+import random
+
+def get_intent(key, url, query):
+
+    params = urllib.parse.urlencode({"query": query})
+
+    response = requests.get(url + params)
+
+    data = response.json()
+
+    
+    intents = data['prediction']['intents']
+    score_max = 0
+    if 'ville' in data['prediction']['entities'].keys():
+        entity = data['prediction']['entities']['ville'][0]
+    else:
+        entity = "No entity"
+    for intent in intents:
+        score = intents[intent]['score']
+        if score > score_max:
+            score_max = score
+            most_likely_intent = intent
+    return {
+        'intent': most_likely_intent,
+        'score': score_max,
+        'entity': entity,
+    }
+
+def get_temp(key, city):
+
+    response = requests.post(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}")
+    response = response.json()
+    if 'main' in response.keys():
+        temp = response["main"]["temp"] - 273.15
+    else:
+        temp = "Bad request"
+    return {
+        'city': city,
+        'temp': temp,
+    }
+
+def get_sunset(key, city):
+
+    response = requests.post(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={key}")
+    data = response.json()
+    datetime.fromtimestamp(1172969203.1)
+    sunset = datetime.fromtimestamp(data['sys']['sunset'])
+    return {
+        'city': city,
+        'sunset': f"{sunset:%Y-%m-%d %H:%M:%S}",
+    }
+
+def select_learner(learners):
+    return random.choice(learners)
